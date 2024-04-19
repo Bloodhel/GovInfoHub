9c8bb83127f3ee739c473075df52fe37102db759
Kanchandeep Singh Bhasin <kanchandeep2003@gmail.com>
2024-04-20 01:18:30 +0530

    requirements update

--- AI - CHATBOT-MAIN/getresponse.py
+++ AI - CHATBOT-MAIN/getresponse.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from googletrans import Translator
-from main import UserPrompt
+from  main import UserPrompt
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    
+)
 translator = Translator()
 
 class UserPrompt(BaseModel):
